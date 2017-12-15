b107b5edea536448138ab3930cd9d12ca230ce21
food869 <food869@naver.com>
2017-12-15 20:31:27 +0900

    관리자 사용자 완료. 뷰페이지 폼만 좀 더 꾸미면 될듯

--- database/db_config.js
+++ database/db_config.js
@@ -1,14 +1,14 @@
-module.exports={
+/*module.exports={
   'host'     : '127.0.0.1',
   'user'     : 'root',
   'port'     : '3306',
   'password' : '0000',
   'database' : 'web'
-};
-/*module.exports={
-    'host' : 'sql12.freemysqlhosting.net',
-    'user' : 'sql12210170',
-    'password' : '5NPY7a3vWl',
-    'port' : '3306',
-    'database' : 'sql12210170'
 };*/
+module.exports={
+    'host' : 'us-cdbr-iron-east-05.cleardb.net',
+    'user' : 'bdb3e7f68e2ac2',
+    'password' : '1f0da87f',
+    'port' : '3306',
+    'database' : 'heroku_a6908582c811842'
+};

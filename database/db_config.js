e2bf0cbcabb148607f950ccade6bba6ac60d696f
food869 <food869@naver.com>
2017-12-15 18:28:33 +0900

    관리자 + index화면 동적으로 생성 잘되는지 확인해봐야댐

--- database/db_config.js
+++ database/db_config.js
@@ -1,4 +1,3 @@
-/*
 module.exports={
   'host'     : '127.0.0.1',
   'user'     : 'root',
@@ -6,11 +5,10 @@ module.exports={
   'password' : '0000',
   'database' : 'web'
 };
-*/
-module.exports={
+/*module.exports={
     'host' : 'sql12.freemysqlhosting.net',
     'user' : 'sql12210170',
     'password' : '5NPY7a3vWl',
     'port' : '3306',
     'database' : 'sql12210170'
-};
+};*/
--- public/firebase/firebase_use.js
+++ public/firebase/firebase_use.js
@@ -4,7 +4,11 @@ function loginout(event) {
         var provider = new firebase.auth.GoogleAuthProvider();
 
         firebase.auth().signInWithPopup(provider).then(function (result) {
-
+            if(result.user.email=="food8123@gmail.com"){
+                window.location.replace("/admin");
+            }else{
+                window.location.replace("/");
+            }
         }).catch(function (error) {
             alert(error.message)
         });
@@ -14,6 +18,7 @@ function loginout(event) {
         } else {
             firebase.auth().signOut().then(function () {
                 alert("로그아웃 되었습니다.");
+                window.location.replace("/");
             }).catch(function (error) {
                 alert(error.message)
             });
@@ -25,7 +30,9 @@ $("#BTN_GOOGLE_LOGIN").click(function () {
         var provider = new firebase.auth.GoogleAuthProvider();
 
         firebase.auth().signInWithPopup(provider).then(function (result) {
-
+            if(result.user.email=="food8123@gmail.com"){
+                window.location.replace("/admin");
+            }
         }).catch(function (error) {
             alert(error.message)
         });
@@ -33,7 +40,15 @@ $("#BTN_GOOGLE_LOGIN").click(function () {
         if (confirm("정말로 로그아웃 하시겠습니까?") == false) {
             e.preventDefault();
         } else {
+            var user = firebase.auth().currentUser;
+            var isAdmin = false;
+            if(user.email=="food8123@gmail.com"){
+                isAdmin = true;
+            }
             firebase.auth().signOut().then(function () {
+                if(isAdmin){
+                    window.location.replace("/");
+                }
                 alert("로그아웃 되었습니다.");
             }).catch(function (error) {
                 alert(error.message)
